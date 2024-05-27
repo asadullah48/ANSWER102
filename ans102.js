07a2f63487bea36c4dcb8ced8fe3f1bc411be59f
asadullah48 <asadullahshafique@hotmail.com>
2024-05-27 10:22:22 +0500

    Initial commit

---
+++ ans102.js
@@ -0,0 +1,7 @@
+// Q 102
+// Calculates the absolute difference between -5 and 5
+var difference = Math.abs(-5 - 5);
+console.log(difference); // Outputs: 10
+// This line shows 
+//the absolute value of the difference between -5 and 5, 
+//ignoring the sign.
---
+++ ans102.ts
@@ -0,0 +1,7 @@
+// Q 102
+// Calculates the absolute difference between -5 and 5
+const difference: number = Math.abs(-5 - 5);
+console.log(difference); // Outputs: 10
+// This line shows 
+//the absolute value of the difference between -5 and 5, 
+//ignoring the sign.
